596f68cfe7a2962e62a8140da331dbbed993ccbd
Vinaysoni1206 <57379287+Vinaysoni1206@users.noreply.github.com>
2020-03-24 15:17:35 +0530

    Add files via upload

---
+++ OS_Assignment.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+int process[20],bursttime[20],priority[20],wait_time[20],turnaround[20];
+float avg_wt,avg_tt;
+printf("Enter the number of processes : ");
+scanf(" 
+
+
+}
+
